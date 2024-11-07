9e77dff8a3c2c5ec2a7c02144224546f8640c2a8
sebastiangulod <sebastian3000ad@gmail.com>
2024-11-07 07:05:41 -0500

    Lógica de enemigos añadida

--- PiratePlatformer/scenes/game/characters/main_character/main_character_collision.gd
+++ PiratePlatformer/scenes/game/characters/main_character/main_character_collision.gd
@@ -14,7 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# Revisamos todas las colisiones
-	for i in character.get_slide_collision_count():
+	for i in character.get_slide_collision_count() and character:
 		# Obtenemos la colisión
 		var collision = character.get_slide_collision(i)
 		# Obtenemos el collider
